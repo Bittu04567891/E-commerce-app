649dfa0e005ff95ee3a46d0a3bac62874b44862d
Bittu <kumarbittu775@gmail.com>
2023-08-18 01:36:38 +0530

    made responsive

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import { Facebook, Instagram, LinkedIn, LocalPhone, LocationOn, MailOutline, Twitter } from "@mui/icons-material"
 import  styled  from "styled-components"
+import { mobile } from "../responsive"
 
 const Container=styled.div`
     display: flex;
+    ${mobile({
+     flexDirection: "column"
+     })}
 
 `
 const Left=styled.div`
@@ -36,6 +40,9 @@ const SocialIcon=styled.div`
 const Center=styled.div`
      flex: 1;
      padding: 20px;
+     ${mobile({
+     display: "none"
+     })}
 
 `
 const Title=styled.h3`
@@ -56,7 +63,9 @@ const ListItem=styled.li`
 const Right=styled.div`
      flex: 1;
      padding: 20px;
-   
+     ${mobile({
+     backgroundColor: "#fff8f8"
+     })}
 
 `
 const ContactItem=styled.div`
